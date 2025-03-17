bb19e819a6da91af9b9b39ba7e7140da778db9d1
wmccd <wmccd9@gmail.com>
2025-03-17 22:37:36 +0000

    versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.wmccd.whatgoeson"
         minSdk = 30
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.6"
+        versionCode = 8
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
